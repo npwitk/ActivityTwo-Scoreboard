fcdeb87ab04fa85cd3584f0168cff518580b377f
Nonprawich I <40120130+npwitk@users.noreply.github.com>
2025-01-13 15:35:21 +0700

    Initial Commit

---
+++ ActivityTwo-Scoreboard/ActivityTwo_ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTwo_ScoreboardApp.swift
+//  ActivityTwo-Scoreboard
+//
+//  Created by Nonprawich I. on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTwo_ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
